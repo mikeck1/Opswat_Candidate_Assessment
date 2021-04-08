0eeb108dcd1c7873ae64cb7a0e14991d3f4a7024
mike kaufman <mikekaufman4@gmail.com>
2021-04-08 02:59:26 +0000

    Added better comments to test.js

--- tests/test.js
+++ tests/test.js
@@ -1,22 +1,22 @@
 /* DEPENDENCIES */
-var expect = require('chai').expect;
-require('mocha-sinon');
+var expect = require('chai').expect;                                                // For sinon
+require('mocha-sinon');                                                             // Test console.log
 const crypto = require('crypto');                                                   // Used to generate file hash
-const fs = require('fs')
-const path = require('path')
+const fs = require('fs')                                                            // To get file stream
+const path = require('path')                                                        // Gets path to file
 
 /* HELPER FILES */
 const printScan = require('../opswat-api/PrintScanner/printScan.js');
 const { getScanByHash, getScanByDataID, uploadFile } = require('../opswat-api/api/opswat.js');
 
-const fileName = "hi2.txt"
+const fileName = "hi2.txt"                                                          // Sample test input (already scanned file)
 const filePath = path.join(path.dirname(__filename), fileName)
 const fileBuffer = fs.readFileSync(filePath)
 
 
 
 // Get expected result
-const testFileName = "test_result2.txt"
+const testFileName = "test_result2.txt"                                             // Expected result from already scanned file
 const testFilePath = path.join(path.dirname(__filename), testFileName)
 const testResult = fs.readFileSync(testFilePath).toString();
 
@@ -46,8 +46,8 @@ describe('testing getScanByHash ', () => {
         this.sinon.stub(console, 'log');
       });
     it('can get scan report by hash', async () => {
-        await testFileHash(fileBuffer,fileName);
-        expect( console.log.calledWith(0+"") ).to.be.true;       
+        await testFileHash(fileBuffer,fileName);                                    // prints difference in string values
+        expect( console.log.calledWith(0+"") ).to.be.true;                          // Expecting to have 0 length difference  
     });
 });
 
@@ -60,16 +60,16 @@ const testById = async (fileBuffer, fileName) => {
 
     try {
         const resByDataId = await getScanByDataID(id);
-        if (resByDataId.data.scan_results.progress_percentage === 100) {   // Check if file done scanning
+        if (resByDataId.data.scan_results.progress_percentage === 100) {            // Check if file done scanning
             const { scan_results } = resByDataId.data;
-            const result =  printScan(scan_results, fileName, true);       // Display cached results
+            const result =  printScan(scan_results, fileName, true);                // Display cached results
         
-            const diff = (diffMe, diffBy) => diffMe.split(diffBy).join('') // Diff the expected with returned
+            const diff = (diffMe, diffBy) => diffMe.split(diffBy).join('')          // Diff the expected with returned
             const C = diff(result+"",testResult+"")
             console.log(C.length+"");
         }
         
-    } catch (err) {                                                        // Cache DNE so continue, otherwise log error and return
+    } catch (err) {                                                                 // Cache DNE so continue, otherwise log error and return
         if (err.response && err.response.status !== 404)
             console.log(err);
 
